e55e17b1bf6838cacf75c1cb4fa65124edac9fc5
MSantos7 <michael_bryan_santos@dlsu.edu.ph>
2017-08-03 11:34:56 +0800

    Add files via upload

---
+++ Main Menu.cpp
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include <math.h>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(void)
+{
+	int opt=1; 
+	do
+	{
+        system("cls");
+        cout << "==============================="   <<'\n';
+        cout << "            Main Menu          "   <<'\n';
+        cout << "==============================="   <<'\n' <<'\n';
+        cout << "1. Sub Menu 1"   <<'\n';
+        cout << "2. Sub Menu 2"   <<'\n';
+        cout << "3. Sub Menu 3"        <<'\n' <<'\n';
+        cout << "4. Exit"                     <<'\n' <<'\n';
+        cout << "-------------------------------"    <<'\n';
+        cout << ">> ";
+		cin>>opt;
+		system("cls");
+		
+		switch(opt)
+		{				
+			case 1:
+				system ("pause");
+				break;
+			case 2:
+				
+				break;
+			case 3:
+				
+				break;
+			case 4:
+				break;
+			default:	
+				cout<<"Invalid input!\n\n";
+				opt = 1;
+				break;
+		}
+	} while(opt >= 1 && opt <= 3);
+
+
+	cout<<"\nThank you for using this program!\n\n";
+	system("pause");
+	return 0;	
+	
+}
