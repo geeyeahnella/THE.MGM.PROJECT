e2c042c014291057cf2b7117f530a8eee3e0a8d0
MaySybal <maysybal@gmail.com>
2017-08-11 22:47:10 +0800

    Updated (#8)
    - menu - fixed it a bit
    - admin - user & password
    - about - placed some info there

--- Main Menu Format.cpp
+++ Main Menu Format.cpp
@@ -1,8 +1,3 @@
-/*
-Author: Santos, Supetran, Sybal
-Subject: LBYEC72 - EB2
-Date: August 9, 2017
-*/
 
 #include <stdlib.h>
 #include <math.h>
@@ -14,6 +9,91 @@ Date: August 9, 2017
 
 using namespace std;
 
+
+int main(void)
+{
+	int opt=1;
+	int name, pass; //change this to char array later
+	do
+	{
+        system("cls");
+        cout << "==============================="   <<'\n';
+        cout << "            Main Menu          "   <<'\n';
+        cout << "==============================="   <<'\n' <<'\n';
+        cout << "1. Admin"   <<'\n';
+        cout << "2. Employee"   <<'\n';
+        cout << "3. About"        <<'\n' <<'\n';
+        cout << "4. Exit"                     <<'\n' <<'\n';
+        cout << "-------------------------------"    <<'\n';
+        cout << ">> ";
+		cin>>opt;
+		
+		switch(opt)
+		{				
+			case 1:
+				system ("cls");
+				cout << "Administration Access\n\n";
+				// log-in
+					cout << "Username: ";
+					cin >> name;
+					
+					if(name==111){
+						cout << "Password: ";
+						cin >> pass;
+						
+						if(pass==123){
+							system ("cls");
+							cout << "Hello Admin.\n\n";
+						}
+						
+						else{
+							cout << "\n\nIncorrect Password\n\n";
+						}						
+					}
+					
+					else{
+						cout << "\n\nUser does not exist\n\n";
+					}
+					
+				system ("pause");
+				break;
+		
+			case 2:
+				system ("cls");
+				system ("pause");
+				break;
+		
+			case 3:
+				system ("cls");
+				cout << "MGM Project\n\n";
+				cout << "Creators:";
+				cout << "\t SANTOS, Michael\n";
+				cout << "\t\t SUPETRAN, Gia\n";
+				cout << "\t\t SYBAL, May\n";
+				cout << "\nSubmitted to:\t Mr. Melvin Cabatuan\n";
+				cout << "\nIn partial fulfillment of requirements for LBYEC72\n";
+				cout << "\nAbout the Program:";
+				cout << "\n*insert program description here*\n\n";
+				system ("pause");
+				break;
+		
+			case 4:
+				break;
+	
+			default:	
+				cout << "\nInvalid input!\n\n";
+				system ("pause");
+				opt = 1;
+				break;
+		}
+	} while(opt >= 1 && opt <= 3);
+
+
+	cout << "\nThank you for using this program!\n\n";
+	system("pause");
+	return 0;	
+	
+=======
 int main()
 { 
 	system("CLS");
